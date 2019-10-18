893dd92a534117eaca7af73f0dac5e52c45c27c1
Woshiajuana <zhigang.chen@owulia.com>
2019-10-18 13:46:17 +0800

    subcode

--- app/router.js
+++ app/router.js
@@ -247,16 +247,16 @@ const apiRoutes =  [
 module.exports = app => {
     const { router, controller, middleware } = app;
     // 获取应用基础信息
-    router.post('/api/v1/app/info', controller.appInfoController.info);
-    // 初始化应用信息
-    router.post('/api/v1/app/init', controller.appInfoController.init);
-    // 管理员用户授权登录
-    // router.post('/api/v1/user-info/login', middleware.oplogMiddleware(), controller.userInfoController.login);
-    // 初始化路由
-    apiRoutes.forEach((item) => {
-        let { path, handler, method } = Object.assign({ method: 'POST' }, item);
-        router[method.toLocaleLowerCase()](path, ...handler(app));
-    });
+    // router.post('/api/v1/app/info', controller.appInfoController.info);
+    // // 初始化应用信息
+    // router.post('/api/v1/app/init', controller.appInfoController.init);
+    // // 管理员用户授权登录
+    // // router.post('/api/v1/user-info/login', middleware.oplogMiddleware(), controller.userInfoController.login);
+    // // 初始化路由
+    // apiRoutes.forEach((item) => {
+    //     let { path, handler, method } = Object.assign({ method: 'POST' }, item);
+    //     router[method.toLocaleLowerCase()](path, ...handler(app));
+    // });
 };
 
 module.exports.apiRoutes = apiRoutes;
