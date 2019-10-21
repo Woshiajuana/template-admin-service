785bf3fb8dd95698afd3be0d4f8373adcf70dfd4
Woshiajuana <zhigang.chen@owulia.com>
2019-10-21 10:45:36 +0800

    subcode

--- app/service/ApiRouteService.js
+++ app/service/ApiRouteService.js
@@ -8,10 +8,10 @@ module.exports = class HandleServer extends Service {
 
     // 初始化
     async init () {
-        const { ctx } = this;
-        const { apiRoutes } = require('./../router');
-        apiRoutes.forEach((item) => { item.source = 'INIT' });
-        await ctx.model.ApiRouteModel.insertMany(apiRoutes, { ordered: false });
+        const { ctx, app } = this;
+        const arrApiRoutes = app.router.arrRoutes;
+        arrApiRoutes.forEach((item) => { item.source = 'INIT' });
+        await ctx.model.ApiRouteModel.insertMany(arrApiRoutes, { ordered: false });
     }
 
     // 创建
